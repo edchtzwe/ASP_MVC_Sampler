5e91b5b90f34e73e367232e4e27baf68e3adcbbf
edchtzwe <7440820@gmail.com>
2016-05-01 23:27:52 +1000

    >>added author.cs >>added readme.txt

---
+++ WebApplication1/Models/author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class author
+    {
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        //a collection that stores all the books in the bookstore
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
